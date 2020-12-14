a54dfa4edec155294daf9aeb583b4fbf70223060
istott <isaac.cloyd@gmail.com>
2020-12-13 17:08:32 -0700

    feat: add user API
    add likes to POD
    
    add dislikes to POD

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,28 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import User from './User';
 
 import './App.css';
 
+const url = 'https://reqres.in/api/users'
+// console.log('user url', url)
+
 function App() {
   const [pod, setPod] = useState({
-    title: 'hello',
+    title: '',
     date: '',
     image: '',
     explanation: ''
   })
+
+  const [users, setUsers] = useState([])
+  console.log('users:', users)
+
+  const [likes, setLikes] = useState(1)
+  const [dislikes, setDislikes] = useState(1)
+
+
+  const getUsers = () => {
+    axios
+      .get(`${url}`)
+      .then(response => {
+        console.log(response)
+        setUsers(response.data.data)
+      }) 
+  }
 
   useEffect(() => {
     const nasaPOD = () => {
       axios
       .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(res => {
-        console.log(res.data)
+        // console.log(res)
         setPod({
           title: res.data.title,
           date: res.data.date,
           image: res.data.url,
           explanation: res.data.explanation
-        })
+        })        
       })
 
       .catch(err => {
@@ -31,11 +51,14 @@ function App() {
     }
 
     nasaPOD()
+    getUsers()
 
   }, [])
 
+  const liked = () => {
+    setLikes(likes + 1)
+  }
 
-  console.log('This is my pod slice of state:', pod)
 
   return (
     <div className="App">
@@ -43,6 +66,24 @@ function App() {
       <h2>{pod.date}</h2>
       <div><img src={pod.image} height='500vh' alt='nasa pic of the day' /></div>
       <p>{pod.explanation}</p>
+
+        <span><div>{likes}</div></span><button onClick={liked}>👍</button>
+
+        <div>{dislikes}</div>
+        <button onClick={() => {
+          setDislikes(dislikes + 1)
+        }}>👎</button>
+
+      <div className='user-container'>
+        {
+          users.map(user => {
+            return(
+              <User key={user.id} details={user} />
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
