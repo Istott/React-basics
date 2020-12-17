a20e44f4868b552ba0e9d444c15d5f7ae9c0069e
istott <isaac.cloyd@gmail.com>
2020-12-16 22:09:53 -0700

    feat: minor tweeks

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Route, Switch} from 'react-router-dom';
+import axios from 'axios';
 
 import NavBar from './NavBar.js';
 import Pod from './NasaPOD/Pod';
@@ -7,8 +8,30 @@ import Users from './Users.js';
 
 import './App.css';
 
+const url = 'https://reqres.in/api/users'
+
 
 function App() {
+  const [users, setUsers] = useState([])
+  // console.log('users:', users)
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  const getUsers = () => {
+    axios
+      .get(`${url}`)
+      .then(response => {
+        console.log(response)
+        setUsers(response.data.data)
+      }) 
+
+      .catch(err => {
+        console.log('error', err)
+      })
+  }
+
 
   return (
     <div className="App">
@@ -16,11 +39,11 @@ function App() {
 
       <Switch>
         <Route path='/users'>
-          <Users/>
+          <Users users={users}/>
         </Route>
 
         <Route path='/'>
-          <Pod/>
+          <Pod users={users}/>
         </Route>
   
       </Switch>
--- practice-app/src/Users.js
+++ practice-app/src/Users.js
@@ -1,38 +1,39 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import User from './User';
 
 
-const url = 'https://reqres.in/api/users'
+// const url = 'https://reqres.in/api/users'
 // console.log('user url', url)
 
-function Users() {
-  const [users, setUsers] = useState([])
-  console.log('users:', users)
+function Users(props) {
+    // console.log('props:', props)
+//   const [users, setUsers] = useState([])
+//   console.log('users:', users)
 
-  useEffect(() => {
-    getUsers()
-  }, [])
+//   useEffect(() => {
+//     getUsers()
+//   }, [])
 
-  const getUsers = () => {
-    axios
-      .get(`${url}`)
-      .then(response => {
-        console.log(response)
-        setUsers(response.data.data)
-      }) 
+//   const getUsers = () => {
+//     axios
+//       .get(`${url}`)
+//       .then(response => {
+//         console.log(response)
+//         setUsers(response.data.data)
+//       }) 
 
-    .catch(err => {
-      console.log('error', err)
-    })
-  }
+//     .catch(err => {
+//       console.log('error', err)
+//     })
+//   }
 
 
   return (
     <div className="App">
       <div className='user-container'>
         {
-          users.map(user => {
+          props.users.map(user => {
             return(
               <User key={user.id} details={user} />
             )
